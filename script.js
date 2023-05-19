b61758ce1151b86ad810e991421c20da45ab3e53
TashtaevUlugbek <tashtaevulugbek@gmail.com>
2023-05-19 19:07:18 +0500

    Hello

---
+++ script.js
@@ -0,0 +1,8 @@
+let hamburger = document.querySelector(".hamburger");
+        let menu = document.querySelector(".menu")
+        
+        hamburger.addEventListener("click", ()=> {
+            console.log("hello");
+           menu.classList.toggle("open")
+        
+})
